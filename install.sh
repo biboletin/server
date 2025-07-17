b193d1147d686d17480e21030fe810d27f3733cf
biboletin <shady_fan@abv.bg>
2025-07-17 19:25:02 +0300

    Refactored install script

--- install.sh
+++ install.sh
@@ -1,88 +1,52 @@
 #!/bin/bash
-
-
-if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root"
-    exit 1
-fi
+set -eou pipefail
 
 source helpers.sh
 
+if [[ $EUID -ne 0 ]]; then
+    error "This script must be run as root"
+    exit 1
+fi
+
 banner
 
-source variables.sh
+# Load environment configuration
+if [ -f ./config/env.sh ]; then
+  source ./config/env.sh
+fi
 
-create_directories() {
-    for dir in "${DIRS[@]}"; do
-    main_message "Create $dir"
-        if [ ! -d "${dir}" ]; then
-            mkdir -p /home/${USER}/$dir
-            chown -R ${USER}:${GROUP} /home/${USER}/${dir}
-        fi
-    done
-}
-
-install_software() {
-    main_message "Installing software"
-    source download.sh
-}
-
-install_composer() {
-    main_message "Install and configure composer"
-    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-    php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-    rm composer-setup.php
-
-    echo 'export PATH="$PATH:$HOME/.config/composer/vendor/bin"' >> ~/.bashrc
-}
+mkdir -p ./logs
+exec > >(tee -a "$LOG_FILE") 2>&1
 
 
-configure_iptables() {
-    main_message "Configure iptables"
-    load_spinner
-    source iptables.sh
-}
+info "Starting installation at $(date)"
 
-configure_apache() {
-    main_message "Configure LAMP"
-    # main_message "NE PRAVI PROMENI V PHP konfiguraciite zarado php 7.4"
-    load_spinner
-    source apache.sh
-}
+# Create home directories if they do not exist
+for dir in "${DIRS[@]}"; do
+	echo "Checking directory: $dir"
+	if [ ! -d "${dir}" ]; then
+		plus_sign "Create $dir"
 
-backup_files() {
-    main_message "Backup files before configuration"
-    source backup_files.sh
-    main_message "All files are with prefix \"backup.\" in their corresponding directory"
-}
+		mkdir -p "${HOME_DIR}/$dir"
+		chown -R ${USER}:${GROUP} "${HOME_DIR}/${dir}"
+	else
+		info "Directory $dir already exists"
+	fi
+done
 
-configure_files() {
-    source configure.sh
-}
+# Run all modules
+for script in ./modules/*.sh; do
+  filename=$(basename "$script")
 
+  # Check if the file is in the exclusion list
+  if [[ " ${EXCLUDED_FILES[@]} " =~ " ${filename} " ]]; then
+    warning "Skipping $filename"
+    continue
+  fi
 
-
-# source permissions.sh
-
-yes | add-apt-repository ppa:ondrej/php
-yes | apt update
-yes | apt upgrade
+  info "Running $filename"
+  bash "$script"
+done
 
 
-# create_directories
-# install_software
-# install_composer
-configure_iptables
-# backup_files
-# configure_apache
-
-# configure_files
-# configure_permissions
-
-# configure_filesystem
-
-
-# yes | apt update
-# yes | apt upgrade
-
-# echo "Generated password for redis: ${PASSWORD}"
+success " Installation complete at $(date)"
